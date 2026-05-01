9d8d88e93e72e7d46a02a7898e6469231f00c3da
GLITCHEDMATR1X <GLITCHEDMATRIXLIVE@GMAIL.COM>
2026-05-01 08:02:19 -0700

    main

--- related_apps/Code_RED_ScriptHookRDR_AI_Menu/build_bridge.bat
+++ related_apps/Code_RED_ScriptHookRDR_AI_Menu/build_bridge.bat
@@ -7,9 +7,6 @@ rem Run from the repository root in a Visual Studio Developer Command Prompt.
 set OUTDIR=related_apps\Code_RED_ScriptHookRDR_AI_Menu\build
 set SRC=related_apps\Code_RED_ScriptHookRDR_AI_Menu\CodeRED_AI_Menu.cpp
 set OUT=%OUTDIR%\CodeRED_AI_Menu.asi
-set SDK=ScriptHookRDR\sdk
-set SDK_INC=%SDK%\inc
-set SDK_LIB=%SDK%\lib\ScriptHookRDR.lib
 set OBJ=%OUTDIR%\CodeRED_AI_Menu.obj
 
 if not exist "%OUTDIR%" mkdir "%OUTDIR%"
@@ -21,12 +18,7 @@ if errorlevel 1 (
   exit /b 1
 )
 
-if not exist "%SDK_LIB%" (
-  echo [ERROR] ScriptHookRDR import library was not found: %SDK_LIB%
-  exit /b 1
-)
-
-cl /std:c++17 /EHsc /LD /nologo /I "%SDK_INC%" /Fo:"%OBJ%" "%SRC%" "%SDK_LIB%" /Fe:"%OUT%" /link /OUT:"%OUT%" /MACHINE:X64
+cl /std:c++17 /EHsc /LD /nologo /Fo:"%OBJ%" "%SRC%" /Fe:"%OUT%" /link /OUT:"%OUT%" /MACHINE:X64
 if errorlevel 1 (
   echo [ERROR] Build failed.
   exit /b 1
